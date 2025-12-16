7a9a10b3b4e56aeaf87076cf90c726e7159b3067
k-simons <k.simons221@gmail.com>
2025-12-16 10:11:10 -0500

    Update function/runnable.go
    Co-authored-by: Nick Miyake <nmiyake@users.noreply.github.com>

--- function/runnable.go
+++ function/runnable.go
@@ -56,7 +56,7 @@ func NewRunnableFromConsumer[T any](arg T, consumer Consumer[T]) Runnable {
 	})
 }
 
-// NewRunnableFromSupplier is a utility function that will create a Runnable given an arg and a Supplier[any]
+// NewRunnableFromSupplier returns a Runnable created from the provided Supplier[R]. The returned Runnable's Run function calls the Get function of the provided Supplier, ignores the R value returned by the function, and returns the error returned by the function.
 func NewRunnableFromSupplier[T any](c Supplier[T]) Runnable {
 	return NewRunnableFromFunc(func(ctx context.Context) error {
 		_, err := c.Get(ctx)
