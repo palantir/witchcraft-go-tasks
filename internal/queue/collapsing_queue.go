f75b3fd5d93b44b7a0007e2a1f146e13745fb7b5
Kevin Simons <ksimons@palantir.com>
2025-12-17 06:34:09 -0800

    Iteration

--- internal/queue/collapsing_queue.go
+++ internal/queue/collapsing_queue.go
@@ -49,20 +49,20 @@ func NewCollapsingQueue[T comparable]() CollapsingQueue[T] {
 	}
 }
 
-type collapsingQueue[t comparable] struct {
+type collapsingQueue[T comparable] struct {
 	// queue defines the order in which we will work on items. Every
 	// element of queue should be in the dirty set and not in the
 	// processing set.
-	queue *queueWrapper[t]
+	queue *queueWrapper[T]
 
 	// dirty defines all of the items that need to be processed.
-	dirty collections.Set[t]
+	dirty collections.Set[T]
 
 	// Things that are currently being processed are in the processing set.
 	// These things may be simultaneously in the dirty set. When we finish
 	// processing something and remove it from this set, we'll check if
 	// it's in the dirty set, and if so, add it to the queue.
-	processing collections.Set[t]
+	processing collections.Set[T]
 
 	cond *sync.Cond
 
